d6994f10508cc1f8057977867af87a998afab834
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2021-05-18 22:41:02 -0300

    Pass values between components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
+
+  valor: number = 0;
+  destino: number = 0;
+
+  transferir($event: any) {
+    console.log("Received Event -> ", $event);
+
+    this.valor = $event.valor;
+    this.destino = $event.destino;
+  }
 }
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector: 'app-nova-transferencia',
@@ -7,8 +7,15 @@ import { Component } from "@angular/core";
 })
 export class NovaTransferenciaComponent {
 
+  @Output() emissor = new EventEmitter<any>();
+
+  valor: number = 0;
+  destino: number = 0;
+
   transferir() {
     console.log("Submit received successfully.");
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.emissor.emit(valorEmitir);
   }
 
 }
